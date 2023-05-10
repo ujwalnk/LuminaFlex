8bb5daf06d8eda0990dd7bafb957cf0b37c129fe
Ujwal N K <ujwal.nk@gmail.com>
2023-05-10 19:10:45 +0530

    Add GammaRamp for brightness control

--- src/autoGlow.cpp
+++ src/autoGlow.cpp
@@ -8,15 +8,34 @@
 #include <Windows.h>
 
 #include "getScreenLuminance.h"
-#include "setScreenBrightness.h"
+#include "gammaramp.h"
 
 #pragma comment(lib, "gdi32.lib")
 
 using namespace std;
 
-int main(){
+int WINAPI WinMain(
+  HINSTANCE hInstance,      // handle to current instance
+  HINSTANCE hPrevInstance,  // handle to previous instance
+  LPSTR lpCmdLine,          // command line
+  int nCmdShow              // show state
+){
 	
-	cout << getScreenLuminance();
+	cout << getScreenLuminance() << endl;
+	
+	CGammaRamp gammaRamp;
+
+	// Make the screen darker
+	gammaRamp.SetBrightness(NULL, 64);
+	
+	cout << "Set Brightness to 50%" << endl;
+
+	Sleep(3000);
+
+	// Return the screen brightness back to normal
+	cout << "Return to normal";
+	gammaRamp.SetBrightness(NULL, 128);
+	
 	return 0;
 }
 
--- src/setScreenBrightness.cpp
+++ src/setScreenBrightness.cpp
@@ -1,5 +1,10 @@
 /**
  * Author: Ujwal N K
  * Created On: 06 May, 2023
- * Set the Screen Brightness
+ * Set the Screen Brightness - Using Windows Command Prompt
 **/
+
+
+#include <iostream>
+
+using namespace std;
