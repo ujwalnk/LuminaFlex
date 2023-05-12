cb00580788e56846f493f10fe5d38bc968fdfab3
Ujwal N K <ujwal.nk@gmail.com>
2023-05-12 15:03:32 +0530

    Refactoring

--- src/gammaramp.h
+++ src/gammaramp.h
@@ -6,25 +6,26 @@
 #ifndef GAMMARAMP_H_
 #define GAMMARAMP_H_
 
-#include <windows.h>
+	#include <windows.h>
 
-class CGammaRamp {
- protected:
-  HMODULE hGDI32;
-  HDC hScreenDC;
-  typedef BOOL(WINAPI *Type_SetDeviceGammaRamp)(HDC hDC, LPVOID lpRamp);
+	class CGammaRamp {
+		protected:
+			HMODULE hGDI32;
+			HDC hScreenDC;
+			typedef BOOL(WINAPI *Type_SetDeviceGammaRamp)(HDC hDC, LPVOID lpRamp);
 
-  Type_SetDeviceGammaRamp pGetDeviceGammaRamp;
-  Type_SetDeviceGammaRamp pSetDeviceGammaRamp;
+			Type_SetDeviceGammaRamp pGetDeviceGammaRamp;
+			Type_SetDeviceGammaRamp pSetDeviceGammaRamp;
 
- public:
-  CGammaRamp();
-  ~CGammaRamp();
-  BOOL LoadLibrary();
-  void FreeLibrary();
-  BOOL LoadLibraryIfNeeded();
-  BOOL SetDeviceGammaRamp(HDC hDC, LPVOID lpRamp);
-  BOOL GetDeviceGammaRamp(HDC hDC, LPVOID lpRamp);
-  BOOL SetBrightness(HDC hDC, WORD wBrightness);
-};
+		public:
+			CGammaRamp();
+			~CGammaRamp();
+			BOOL LoadLibrary();
+			void FreeLibrary();
+			BOOL LoadLibraryIfNeeded();
+			BOOL SetDeviceGammaRamp(HDC hDC, LPVOID lpRamp);
+			BOOL GetDeviceGammaRamp(HDC hDC, LPVOID lpRamp);
+			BOOL SetBrightness(HDC hDC, WORD wBrightness);
+	};
+	
 #endif
