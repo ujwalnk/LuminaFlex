276d974c1aee7542efc39abe3056d74b835ab68d
Ujwal N K <ujwal.nk@gmail.com>
2023-05-12 14:58:25 +0530

    Refactor consoleSentinel & fix setBrightness

---
+++ src/consoleSentinel.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+#include "consoleSentinel.h"
+
+using namespace std;
+
+/**
+ * Execute cmd in Windows Cmd
+ * @param (const char*) cmd: command to execute
+ */
+string ConsoleSentinel::exec(const char* cmd) {
+    char buffer[128];
+    string result = "";
+
+    // Open a pipe in read mode
+    FILE* pipe = popen(cmd, "r");
+
+    // Unable to Open pipe, throw error
+    if (!pipe) throw runtime_error("popen() failed!");
+
+    // Read contents line by line & append to buffer, result
+    try {
+    while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+        result += buffer;
+    }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+
+    // Close pipe & return
+    pclose(pipe);
+    return result;
+}
+
+/**
+ * Get the current system brightness
+ * @return (int): Brightness level of system
+*/
+int ConsoleSentinel::getBrightness() {
+    return atoi(exec("powershell -Command \"Get-Ciminstance -Namespace root/WMI "
+                   "-ClassName WmiMonitorBrightness | Select -ExpandProperty "
+                   "\"CurrentBrightness\"\"")
+                  .c_str());
+}
+
+/**
+ * Set system brightness
+ * @param (int) level: Brightness level
+*/
+void ConsoleSentinel::setBrightness(int level) {
+    stringstream ss;
+    ss << "powershell (Get-WmiObject -Namespace root/WMI -Class "
+        << "WmiMonitorBrightnessMethods).WmiSetBrightness(1," << level << ')';
+    exec(ss.str().c_str());
+}
---
+++ src/consoleSentinel.h
@@ -0,0 +1,16 @@
+#ifndef consoleSentinel_h
+#define consoleSentinel_h
+
+    #ifndef string
+        #include <string>
+    #endif
+
+    class ConsoleSentinel{
+        private:
+            std::string exec(const char* cmd);
+        public:
+            int getBrightness();
+            void setBrightness(int level);
+    };
+
+#endif
